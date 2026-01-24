9ac62c17392e778bab87b341a8031874cba1b6c3
imvivek123 <147731166+imvivek123@users.noreply.github.com>
2026-01-24 13:33:06 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical-chat-bot",
+    version="0.1.0",
+    author="Vishwendra Singh",
+    author_email="vishwendra091@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
